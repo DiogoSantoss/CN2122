a99fc22984e9b50fa8399b86d7a1caf5531b549e
miguelporfirio19 <miguelporfirio64@gmail.com>
2021-12-02 19:40:35 +0000

    Added flags for parsing

--- Project/User.c
+++ Project/User.c
@@ -9,15 +9,21 @@
 #include <string.h>
 
 void parseArguments(int argc, char *argv[]){
-    int count = 0;
+    int count_n = 0;
+    int count_p = 0;
 
     for(int i = 1; i<argc; i++){
-        if(argv[i]=="-n" || argv[i]=="-p"){
-            count++;
+        if(strcmp(argv[i],"-n")==0){
+            count_n++;
+        }
+        if(strcmp(argv[i],"-p")==0){
+            count_p++;
         }
     }
-    if
-
+    if(count_n != 1 || count_p != 1){
+        fprintf(stderr, "Invalid format\n");
+        exit(1);
+    }
 }
 
 int main(int argc, char *argv[]){
