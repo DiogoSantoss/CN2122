594488f48013fecf0aadbadf6cb23d8c36b0e0eb
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2021-12-11 04:09:09 +0000

    added colors.c and colors.h :smile: :sunglasses: :thumbsup:

---
+++ Project/User/colors.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Pretty colors
+void red() {
+  printf("\033[1;31m");
+}
+
+void green() {
+  printf("\033[0;32m");
+}
+
+void blue() {
+  printf("\033[0;34m");
+}
+
+void purple() {
+  printf("\033[0;34m");
+}
+
+void cyan() {
+  printf("\033[0;36m");
+}
+
+void white() {
+  printf("\033[0;37m");
+}
+
+void yellow() {
+  printf("\033[1;33m");
+}
+
+void reset() {
+  printf("\033[0m");
+}
---
+++ Project/User/colors.h
@@ -0,0 +1,13 @@
+#ifndef COLORS_DOT_H
+#define COLORS_DOT_H
+
+void red();
+void green();
+void blue();
+void purple();
+void cyan();
+void white();
+void yellow();
+void reset();
+
+#endif
