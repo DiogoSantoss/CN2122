75608a189447c66290929b7fede63aa4b34100ed
franciscomcsousa <francisco.m.c.sousa@gmail.com>
2021-12-29 20:50:42 +0000

    User folder added to server (not checking well if dupes exists in register)

--- Project/Server/requestsUDP.c
+++ Project/Server/requestsUDP.c
@@ -3,10 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <errno.h>
 
 #include "log.h"
 #include "structs.h"
 #include "common.h"
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int mkdir(const char *pathname, mode_t mode);
 
 // Booleans
 #define TRUE  1
@@ -14,6 +21,57 @@
 
 #define MAXSIZEUDP 39 //GSR is the biggest command 
 #define EXTRAMAXSIZE 3169
+
+int CreateUserDir(char *UID){
+
+    char user_dirname[20];
+    int ret;
+
+    sprintf(user_dirname,"Users/%s",UID);
+    ret=mkdir(user_dirname,0700);
+
+    if(ret==-1)
+        return FALSE;
+
+    return TRUE;
+}
+
+int CreateUserPassword(char* UID, char* password){
+
+    FILE *fptr;
+    char path[31];
+    sprintf(path, "Users/%s/%s_password.txt", UID, UID);
+
+    if(!(fptr = fopen(path, "w"))){
+        //Failed to open path
+        return FALSE;
+    }
+
+    fwrite(password, sizeof(char), 8, fptr);
+    fclose(fptr);
+
+    return TRUE;
+}
+
+int UserExists(char* UID){
+
+    char path[31];
+    DIR* dir;
+    sprintf(path, "Users/%s", UID);
+    if(dir = opendir(path)){
+        // User exists
+        return TRUE;
+    }
+    else if (ENOENT == errno) {
+        //Directory doesnt exist.
+        return FALSE;
+    } 
+    else {
+        //Unknown error
+        return FALSE;
+    }
+}
+
 
 char* processREG(userData user, serverData server, int fd, char* request){
 
@@ -25,15 +83,23 @@ char* processREG(userData user, serverData server, int fd, char* request){
     sscanf(request, "%s %s %s %s", prefix, UserID, password, sufix);
 
     if (strlen(sufix) != 0 || strlen(UserID) != 5 || strlen(password) != 8){
-        //logError("Wrong size parameters.");
+        //Wrong size parameters
         strcpy(message, "RRG NOK\n");
         return NULL;
     }
     else if (!checkStringIsNumber(UserID) || !checkStringIsAlphaNum(password)){
-        //logError("Forbidden character in parameters.");
+        //Forbidden character in parameters
         strcpy(message, "RRG NOK\n");
         return NULL;
     }
+    else if (UserExists(UserID)){
+        // User exists
+        strcpy(message, "RRG DUP");
+    }
+
+    if (!CreateUserDir(UserID)) strcpy(message, "ERR");
+    if (!CreateUserPassword(UserID, password)) strcpy(message, "ERR");
+
     strcpy(message, "RRG OK\n");
     return message;
 }
